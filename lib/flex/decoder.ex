1229c4d3478aa10b7eb965971917a03be27605e5
Dennis Dang <dangggdennis@gmail.com>
2021-03-22 08:39:21 -0400

    Decode [u]intX and [u]fix values

--- lib/flex/decoder.ex
+++ lib/flex/decoder.ex
@@ -16,8 +16,95 @@ defmodule Flex.Decoder do
     end
   end
 
+  def decode(%{"type" => "Bool", "value" => value}) do
+    value
+  end
+
+  def decode(%{"type" => "String", "value" => value}) do
+    value
+  end
+
+  def decode(%{"type" => "Address", "value" => value}) do
+    value
+  end
+
   def decode(%{"type" => "Int", "value" => val}) do
-    IO.puts("decoding integer")
     String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int8", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt8", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int16", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt16", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int32", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt32", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int64", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt64", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int128", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt128", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Int256", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "UInt256", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Word8", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Word16", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Word32", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Word64", "value" => val}) do
+    String.to_integer(val)
+  end
+
+  def decode(%{"type" => "Fix64", "value" => val}) do
+    String.to_float(val)
+  end
+
+  def decode(%{"type" => "UFix64", "value" => val}) do
+    String.to_float(val)
   end
 end
--- test/flex_test/decoder_test.exs
+++ test/flex_test/decoder_test.exs
@@ -24,4 +24,179 @@ defmodule FlexTest.Decoder do
            }
            |> Decoder.decode() == nil
   end
+
+  test "optional non-nil" do
+    assert %{
+             "type" => "Optional",
+             "value" => %{
+               "type" => "Int8",
+               "value" => "123"
+             }
+           }
+           |> Decoder.decode() == 123
+  end
+
+  test "boolean true" do
+    assert %{
+             "type" => "Bool",
+             "value" => true
+           }
+           |> Decoder.decode() == true
+  end
+
+  test "boolean false" do
+    assert %{
+             "type" => "Bool",
+             "value" => false
+           }
+           |> Decoder.decode() == false
+  end
+
+  test "string empty" do
+    assert %{
+             "type" => "String",
+             "value" => ""
+           }
+           |> Decoder.decode() == ""
+  end
+
+  test "string exist" do
+    assert %{
+             "type" => "String",
+             "value" => "snargle-blob"
+           }
+           |> Decoder.decode() == "snargle-blob"
+  end
+
+  test "address" do
+    assert %{
+             "type" => "Address",
+             "value" => "0x1234"
+           }
+           |> Decoder.decode() == "0x1234"
+  end
+
+  test "[U]Int, [U]Int8, [U]Int16, [U]Int32,[U]Int64,[U]Int128, [U]Int256,  Word8, Word16, Word32, or Word64" do
+    assert %{
+             "type" => "Int",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int8",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt8",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int16",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt16",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int32",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt32",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int64",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt64",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int128",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt128",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Int256",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "UInt256",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Word8",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Word16",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Word32",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+
+    assert %{
+             "type" => "Word64",
+             "value" => "1"
+           }
+           |> Decoder.decode() == 1
+  end
+
+  test "[U]Fix64" do
+    assert %{
+             "type" => "Fix64",
+             "value" => "1312.41212"
+           }
+           |> Decoder.decode() == 1312.41212
+
+    assert %{
+             "type" => "UFix64",
+             "value" => "-1312.4121299999999"
+           }
+           |> Decoder.decode() == -1312.4121299999999
+  end
 end
