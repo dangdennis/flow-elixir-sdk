605360c2ee363328ba1091c6facc17974a7ed3a4
Dennis Dang <dangggdennis@gmail.com>
2021-03-22 08:11:37 -0400

    Prep work to publish to hex

--- mix.exs
+++ mix.exs
@@ -6,8 +6,12 @@ defmodule Flex.MixProject do
       app: :flex,
       version: "0.1.0",
       elixir: "~> 1.11",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: """
+      The Elixir client SDK for the Flow blockchain.
+      """
     ]
   end
 
@@ -18,14 +22,22 @@ defmodule Flex.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Dennis Dang"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://https://github.com/dangdennis/flow-elixir-sdk"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, "~> 1.2"},
-      {:grpc, github: "elixir-grpc/grpc"},
-      {:google_protos, "~> 0.1"},
-      {:protobuf, github: "tony612/protobuf-elixir", override: true},
-      {:cowlib, "~> 2.9.0", override: true},
+      #   {:grpc, github: "elixir-grpc/grpc"},
+      #   {:google_protos, "~> 0.1"},
+      #   {:protobuf, github: "tony612/protobuf-elixir", override: true},
+      #   {:cowlib, "~> 2.9.0", override: true},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
